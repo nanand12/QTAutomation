449176211170831f4d335c569a9008b9fda5264a
ramya.ms <ramya.ms@QT-INBG-LT1137.QUALITEST.LOCAL>
2022-08-18 11:54:19 +0530

    commiting my changes

--- src/test/java/selenium/QTAutomation/samplepush.java
+++ src/test/java/selenium/QTAutomation/samplepush.java
@@ -3,8 +3,8 @@ package selenium.QTAutomation;
 public class samplepush {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("pushing this new1 changes");
+		System.out.println("pushing this new2 changes");
 	}
 
 }
